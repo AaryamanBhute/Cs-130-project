5164d9026a5cfde22f083f79e55f1c32e83f253e
Noah Gardner <noahgardner303@gmail.com>
2023-11-08 21:24:22 -0800

    basic website

---
+++ src/Chess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Chess = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Chess</h1>
+      <p style={{ textAlign: 'center' }}>
+        <a href="/">Back to Home</a>
+      </p>
+    </div>
+  );
+};
+
+export default Chess;
---
+++ src/Yahtzee.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Yahtzee = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Yahtzee</h1>
+      <p style={{ textAlign: 'center' }}>
+        <a href="/">Back to Home</a>
+      </p>
+    </div>
+  );
+};
+
+export default Yahtzee;
