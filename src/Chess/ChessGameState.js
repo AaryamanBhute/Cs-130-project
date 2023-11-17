5cb363a877f316ebe53bfd9ea48b6fd639b6d22b
Thundersquirrels <carson@reality.net>
2023-11-17 00:40:02 -0800

    Added check and checkmate detection, refactored code, and improved display

--- src/Chess/ChessGameState.js
+++ src/Chess/ChessGameState.js
@@ -1,5 +1,6 @@
 // GameState.js
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { validateMove } from './ValidateMove';
 import produce from 'immer';
 
 const getRandomBoolean = () => Math.random() < 0.5;
@@ -24,6 +25,9 @@ const createInitialBoard = () => {
 
   return {
     board: orientedRows,
+    blackCapturedPieces: [],
+    whiteCapturedPieces: [],
+    currentPlayer: 'w',
     whiteOnBottom: isWhiteOnBottom,
     lastMove: {},
     specialRequirements: {
@@ -45,55 +49,158 @@ export const useChessGameState = () => {
     setGameState(newGameState);
   };
 
-  const movePiece = (piece, startRow, startCol, endRow, endCol, specialRequirements) => {
-    setGameState(() => {
-      let newGameState = produce(gameState, (draft) => {
-        draft.board[startRow][startCol] = "";
-        if (piece && piece[1] === 'p' && (endRow === 0 || endRow === 7)) {
-          draft.board[endRow][endCol] = piece[0] + 'queen';
+  const movePiece = React.useCallback((gameState, move) => {
+    const piece = move.piece;
+    const startRow = move.startRow;
+    const startCol = move.startCol;
+    const endRow = move.endRow;
+    const endCol = move.endCol;
+    const specialRequirements = move.specialRequirements;
+    return produce(gameState, (draft) => {
+      draft.board[startRow][startCol] = "";
+      const capturedPiece = draft.board[endRow][endCol];
+      if (capturedPiece) {
+        if (capturedPiece[0] === 'w') {
+          draft.blackCapturedPieces.push(capturedPiece);
         } else {
-          draft.board[endRow][endCol] = piece;
+          draft.whiteCapturedPieces.push(capturedPiece);
         }
-        draft.lastMove = { piece, startRow, startCol, endRow, endCol };
-        draft.specialRequirements = specialRequirements;
-      });
-      return newGameState;
-    });
-  };
-
-  const castle = (castlePiece, castleStartRow, castleStartCol, castleEndRow, castleEndCol, piece, startRow, startCol, endRow, endCol, specialRequirements) => {
-    setGameState(() => {
-      const newGameState = produce(gameState, (draft) => {
-        draft.board[castleStartRow][castleStartCol] = "";
-        draft.board[castleEndRow][castleEndCol] = castlePiece;
-        draft.board[startRow][startCol] = "";
+      }
+      if (piece && piece[1] === 'p' && (endRow === 0 || endRow === 7)) {
+        draft.board[endRow][endCol] = piece[0] + 'queen';
+      } else {
         draft.board[endRow][endCol] = piece;
-        draft.lastMove = { piece, startRow, startCol, endRow, endCol };
-        draft.specialRequirements = specialRequirements;
-      });
-      return newGameState;
+      }
+      draft.currentPlayer = piece[0] === 'w' ? 'b' : 'w';
+      draft.lastMove = { piece, startRow, startCol, endRow, endCol };
+      draft.specialRequirements = specialRequirements;
     });
-  };
+  }, []); // Add any dependencies here
 
-  const enPassant = (piece, startRow, startCol, endRow, endCol, specialRequirements) => {
-    setGameState(() => {
-      const newGameState = produce(gameState, (draft) => {
-        draft.board[gameState.lastMove.endRow][gameState.lastMove.endCol] = "";
-        draft.board[startRow][startCol] = "";
-        draft.board[endRow][endCol] = piece;
-        draft.lastMove = { piece, startRow, startCol, endRow, endCol };
-        draft.specialRequirements = specialRequirements;
-      });
-      return newGameState;
+  const castle = React.useCallback((gameState, move) => {
+    const piece = move.piece;
+    const startRow = move.startRow;
+    const startCol = move.startCol;
+    const endRow = move.endRow;
+    const endCol = move.endCol;
+    const castlePiece = piece[0] + 'rook';
+    const castleStartRow = startRow;
+    const castleStartCol = endCol === 1 ? 0 : 7;
+    const castleEndRow = startRow;
+    const castleEndCol = endCol === 1 ? 2 : 5;
+    const specialRequirements = move.specialRequirements;
+    return produce(gameState, (draft) => {
+      draft.board[castleStartRow][castleStartCol] = "";
+      draft.board[castleEndRow][castleEndCol] = castlePiece;
+      draft.board[startRow][startCol] = "";
+      draft.board[endRow][endCol] = piece;
+      draft.currentPlayer = piece[0] === 'w' ? 'b' : 'w';
+      draft.lastMove = { piece, startRow, startCol, endRow, endCol };
+      draft.specialRequirements = specialRequirements;
     });
-  };
+  }, []); // Add any dependencies here
+
+  const enPassant = React.useCallback((gameState, move) => {
+    const piece = move.piece;
+    const startRow = move.startRow;
+    const startCol = move.startCol;
+    const endRow = move.endRow;
+    const endCol = move.endCol;
+    const lastMove = gameState.lastMove;
+    const specialRequirements = move.specialRequirements;
+    return produce(gameState, (draft) => {
+      let capturedPiece = draft.board[lastMove.endRow][lastMove.endCol];
+      if (capturedPiece) {
+        if (capturedPiece[0] === 'w') {
+          draft.blackCapturedPieces.push(capturedPiece);
+        } else {
+          draft.whiteCapturedPieces.push(capturedPiece);
+        }
+      }
+      draft.board[lastMove.endRow][lastMove.endCol] = "";
+      draft.board[startRow][startCol] = "";
+      draft.board[endRow][endCol] = piece;
+      draft.currentPlayer = piece[0] === 'w' ? 'b' : 'w';
+      draft.lastMove = { piece, startRow, startCol, endRow, endCol };
+      draft.specialRequirements = specialRequirements;
+    });
+  }, []); // Add any dependencies here
+
+  const makeMove = React.useCallback((gameState, move) => {
+    // Update the game state based on the move
+    if (move.castle) {
+      return castle(gameState, move);
+    } else if (move.enPassant) {
+      return enPassant(gameState, move);
+    } else {
+      return movePiece(gameState, move);
+    }
+  }, [castle, enPassant, movePiece]); // Add any dependencies here
+  
+  const getValidMoves = React.useCallback((gameState, player) => {
+    let validMoves = [];
+    for (let startRow = 0; startRow < 8; startRow++) {
+      for (let startCol = 0; startCol < 8; startCol++) {
+        const piece = gameState.board[startRow][startCol];
+        if (piece && piece[0] === player) {
+          for (let endRow = 0; endRow < 8; endRow++) {
+            for (let endCol = 0; endCol < 8; endCol++) {
+              const move = validateMove(gameState, startRow, startCol, endRow, endCol);
+              if (move.valid) {
+                validMoves.push(move);
+              }
+            }
+          }
+        }
+      }
+    }
+    return validMoves;
+  }, []); // Add any dependencies here
+
+  const getKingPosition = React.useCallback((gameState, player) => {
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        const piece = gameState.board[row][col];
+        if (piece && piece[0] === player && piece[1] === 'k' && piece[2] === 'i') {
+          return [row, col];
+        }
+      }
+    }
+  }, []); // Add any dependencies here
+
+  const kingUnderAttack = React.useCallback((gameState, player) => {
+    let kingPosition = getKingPosition(gameState, player);
+    const opponentMoves = getValidMoves(gameState, player === 'w' ? 'b' : 'w');
+    if (opponentMoves.some(move => move.endRow === kingPosition[0] && move.endCol === kingPosition[1])) {
+      return true;
+    }
+    return false;
+  }, [getKingPosition, getValidMoves]); // Add any dependencies here
+
+  React.useEffect(() => {
+    // Check if there's any move that would not result in a check
+    if (kingUnderAttack(gameState, gameState.currentPlayer)) {
+      const validMoves = getValidMoves(gameState, gameState.currentPlayer);
+      for (let move of validMoves) {
+        if (!kingUnderAttack(makeMove(gameState, move), gameState.currentPlayer)) {
+          console.log(`${gameState.currentPlayer === 'w' ? 'White' : 'Black'} is in Check!`);
+          return;
+        }
+      }
+      console.log(`${gameState.currentPlayer === 'w' ? 'White' : 'Black'} wins by Checkmate!`);
+    }
+  }, [gameState, makeMove, kingUnderAttack, getValidMoves]); // Run this effect whenever gameState changes  
+
+  const updateGameState = React.useCallback((gameState, move) => {
+    const newGameState = makeMove(gameState, move);
+    setGameState(newGameState);
+  }, [makeMove]); // Add any dependencies here
 
   return {
     gameState,
     setGameState,
-    movePiece,
-    castle,
-    enPassant,
+    updateGameState,
+    getValidMoves,
     resetGame,
   };
 };
--- src/Chess/Chessboard.js
+++ src/Chess/Chessboard.js
@@ -4,52 +4,46 @@ import { useChessGameState } from './ChessGameState';
 import { validateMove } from './ValidateMove';
 
 const Chessboard = () => {
-  const { gameState, movePiece, castle, enPassant, resetGame } = useChessGameState();
+  const { gameState, updateGameState, getValidMoves, resetGame } = useChessGameState();
   const [selectedSquare, setSelectedSquare] = useState(null);
-  const [currentPlayer, setCurrentPlayer] = useState('w');
+  const [validMoves, setValidMoves] = useState([]);
   const squareSize = 80;
 
   const handleSquareClick = (row, col) => {
     if (selectedSquare) {
       // If selected square is clicked again, deselect it
       if (selectedSquare.row === row && selectedSquare.col === col) {
+        setValidMoves([]);
         setSelectedSquare(null);
         return;
       }
       // If a square is already selected, try to move the piece
       const move = validateMove(gameState, selectedSquare.row, selectedSquare.col, row, col);
-      const piece = gameState.board[selectedSquare.row][selectedSquare.col];
       if (!move.valid) {
         return;
       }
-      console.log(move);
-      if (move.castle.length > 0) {
-        castle(move.castle[0], move.castle[1], move.castle[2], move.castle[3], move.castle[4],
-               piece, selectedSquare.row, selectedSquare.col, row, col, move.specialRequirements);
-      }
-      else if (move.enPassant.length > 0) {
-        console.log("enPassant");
-        enPassant(piece, selectedSquare.row, selectedSquare.col, row, col, move.specialRequirements);
-      }
-      else {
-        movePiece(piece, selectedSquare.row, selectedSquare.col, row, col, move.specialRequirements);
-      }
-      setSelectedSquare(null); // Reset the selected square after moving
-      // After a valid move, switch the current player
-      setCurrentPlayer(currentPlayer === 'w' ? 'b' : 'w');
-    } else {
+      // Update the game state based on the move
+      updateGameState(gameState, move);
+      setValidMoves([]);
+      setSelectedSquare(null);
+    }
+    else {
       // If no square is selected, set the clicked square as the selected square
       const piece = gameState.board[row][col];
-      if (piece && piece[0] === currentPlayer) { // Check if the piece exists and is the correct color
-        setSelectedSquare({row, col});
+      if (piece && piece[0] === gameState.currentPlayer) { // Check if the piece exists and is the correct color
+        setSelectedSquare({ row, col });
+        setValidMoves(getValidMoves(gameState, gameState.currentPlayer));
       }
     }
   };
 
   // Your existing code for rendering a single square
   const renderSquare = (row, col, piece) => {
-    // Calculate background color based on row and column
-    const backgroundColor = (row + col) % 2 === 0 ? '#e8caa2' : '#523106';
+    // Check if the square is a valid move
+    const isHighlighted = validMoves.some(move => selectedSquare.row === move.startRow && selectedSquare.col === move.startCol
+                                                  && move.endRow === row && move.endCol === col);
+    // Calculate background color based on row and column and highlight the square if it's a valid move
+    const backgroundColor = isHighlighted ? '#83dbee' : (row + col) % 2 === 0 ? '#e8caa2' : '#523106';
 
     // Set the background color inline
     const squareStyle = {
@@ -81,6 +75,41 @@ const Chessboard = () => {
     );
   };
 
+  const CapturedPieces = ({ player, pieces }) => {
+    return (
+      <div style={{ margin: 'auto' }}>
+        <h2>{player}'s Captured Pieces:</h2>
+        <ul>
+          {pieces.map((piece, index) => (
+              <img
+                src={require(`../assets/${piece}.png`)} // Update the path based on your asset structure
+                alt={piece}
+                style={{ width: `${squareSize/2}px`, height: `${squareSize/2}px` }}
+              />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  const Timer = () => {
+    const [seconds, setSeconds] = useState(0);
+  
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+  
+    return (
+      <div>
+        <h2>Game Time:</h2>
+        <p>{seconds} seconds</p>
+      </div>
+    );
+  }
+
   const handleResetClick = () => {
     resetGame();
     setSelectedSquare(null);
@@ -90,22 +119,32 @@ const Chessboard = () => {
   const boardWidth = gameState.board[0].length * squareSize;
 
   return (    
-  <div style={{backgroundColor: '#6495ED', height: '100vh' }}>
-    <h1 style={{ marginTop: '0px', textAlign: 'center', color: '#eeeeee' }}>Chess</h1>
-    <p style={{ marginBottom: '20px', margin: 'auto', height: '15px', width: '100px', font: 'bold 11px Arial', textDecoration: 'none', backgroundColor: '#eeeeee', padding: '2px 6px 2px 6px', borderTop: '1px solid #CCCCCC', borderRight: '1px solid #333333', borderBottom: '1px solid #333333', borderLeft: '1px solid #CCCCCC', textAlign: 'center', color: '#eeeeee' }}>
-      <a href="/">Back to Home</a>
-    </p>
-    <div className="chessboard-container" style={{ width: `${boardWidth}px`, margin: 'auto' }}>
-      {/* Render each row */}
-      {gameState.board.map((row, rowIndex) => (
-        <div key={rowIndex} style={{ display: 'flex' }}>
-          {/* Render squares for each row */}
-          {row.map((piece, colIndex) => renderSquare(rowIndex, colIndex, piece))}
-        </div>
-      ))}
-
-      {/* Example: Reset button */}
-      <button style={{ marginTop: '20px', height: '40px' }}onClick={handleResetClick}>Reset Chess Game</button>
+  <div style={{ backgroundColor: '#6495ED', height: '100vh' }}>
+    <h1 style={{ marginTop: '0px', paddingTop: '40px', font: 'bold 50px Arial', textAlign: 'center', color: '#eeeeee' }}>Chess</h1>
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', margin: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '50px' }}>
+        <CapturedPieces player={gameState.whiteOnBottom ? 'Black' : 'White'} pieces={gameState.whiteCapturedPieces} />
+        <CapturedPieces player={gameState.whiteOnBottom ? 'White' : 'Black'} pieces={gameState.blackCapturedPieces} />
+      </div>
+      <div className="chessboard-container" style={{ width: `${boardWidth}px`, border: '10px solid black', margin: 'auto', marginTop: '10px' }}>
+        {/* Render each row */}
+        {gameState.board.map((row, rowIndex) => (
+          <div key={rowIndex} style={{ display: 'flex' }}>
+            {/* Render squares for each row */}
+            {row.map((piece, colIndex) => renderSquare(rowIndex, colIndex, piece))}
+          </div>
+        ))}
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '50px' }}>
+        <Timer />
+      </div>
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '50px', margin: '20px' }}>
+      <button style={{ border: '1px solid black', height: '50px', font: 'bold 30px Arial' }} 
+              onClick={handleResetClick}>Restart Game</button>
+      <button style={{ border: '1px solid black', height: '50px', font: 'bold 30px Arial' }}>
+        <a href="/" style={{ textDecoration: 'none', color: 'black' }}>Back to Home</a>
+      </button>
     </div>
   </div>
   );
