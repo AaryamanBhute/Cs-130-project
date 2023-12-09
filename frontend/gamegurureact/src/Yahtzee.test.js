740b5e58ba71fb7d1b593be8c63dcf72652e9bf8
Noah Gardner <noahgardner303@gmail.com>
2023-12-08 18:57:05 -0800

    Finished my games' tests

--- frontend/gamegurureact/src/Yahtzee.test.js
+++ frontend/gamegurureact/src/Yahtzee.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, getByAltText } from '@testing-library/react';
 import Yahtzee from './Yahtzee';
+import '@testing-library/jest-dom';
+
 const { calculateScores } = require('./Yahtzee');
 
 test('calculateScores calculates scores correctly', () => {
@@ -11,7 +13,44 @@ test('calculateScores calculates scores correctly', () => {
   });
 
 describe('Yahtzee Component', () => {
-  test('renders Yahtzee component correctly', () => {
-    render(<Yahtzee />);
-  });
+    test('renders Yahtzee component correctly', () => {
+        render(<Yahtzee />);
+        const headingElement = screen.getByText('Yahtzee');
+        expect(headingElement).toBeInTheDocument();
+    });
+    test('Rolling the dice updates roll count', () => {
+        const { getByText } = render(<Yahtzee />);
+        const rollDiceButton = getByText('Roll Dice (3 roll(s) left)');
+        fireEvent.click(rollDiceButton);
+        const updatedRollDiceButton = getByText('Roll Dice (2 roll(s) left)');
+        expect(updatedRollDiceButton).toBeInTheDocument();
+    });
+    test('Rolling the dice displays the dice values', () => {
+        const { getByText } = render(<Yahtzee />);
+        const rollDiceButton = getByText('Roll Dice (3 roll(s) left)');
+        fireEvent.click(rollDiceButton);
+        const diceImages = [
+            screen.getByAltText('Dice 1'),
+            screen.getByAltText('Dice 2'),
+            screen.getByAltText('Dice 3'),
+            screen.getByAltText('Dice 4'),
+            screen.getByAltText('Dice 5'),
+        ];
+        diceImages.forEach((diceImage) => {
+            expect(diceImage).toBeInTheDocument();
+        });
+    });
+    test('Playing a turn updates the score', () => {
+        const { getByText } = render(<Yahtzee />);
+        const rollDiceButton = getByText('Roll Dice (3 roll(s) left)');
+        fireEvent.click(rollDiceButton);
+        const chanceCategoryButton = getByText((content, element) => {
+            return element.tagName.toLowerCase() === 'button' && content.includes('Chance');
+        });
+        fireEvent.click(chanceCategoryButton);
+        const totalScoreElement = screen.getByText(/^Total Score:/);
+        const totalScoreText = totalScoreElement.textContent;
+        const totalScore = parseInt(totalScoreText.replace('Total Score: ', ''), 10);
+        expect(totalScore).not.toBe(0);
+    });    
 });
--- frontend/gamegurureact/src/jest.config.js
+++ frontend/gamegurureact/src/jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   testEnvironment: 'jsdom',
-    transformIgnorePatterns: ["node_modules/(?!axios)"],
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
+  transformIgnorePatterns: [
+    "node_modules/(?!axios)",
+    "\\.css$",
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(png|jpg|jpeg|gif|svg)$': './mockImageFile.js', // Mock image files
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$",
+  testPathIgnorePatterns: ["/node_modules/", "\\.css$"]
 };
